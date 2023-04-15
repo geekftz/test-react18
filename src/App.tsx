c119782ddfc59ac84d565689629266a39ce66c05
hackftz <472849306@qq.com>
2023-04-15 12:01:43 +0800

    feat: add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { useEffect, useCallback, useMemo, useState } from "react";
+import {
+  useEffect,
+  useCallback,
+  useMemo,
+  useState,
+  useLayoutEffect,
+} from "react";
 
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [state, setState] = useState("hello world");
+
+  // useEffect(() => {
+  //   let i = 0;
+  //   while (i <= 100000000) {
+  //     i++;
+  //   }
+  //   setState("world hello");
+  // }, []);
+
+  useLayoutEffect(() => {
+    let i = 0;
+    while (i <= 100000000) {
+      i++;
+    }
+    setState("world hello");
+  }, []);
+
+  return <div className="App">{state}</div>;
 }
 
 export default App;
