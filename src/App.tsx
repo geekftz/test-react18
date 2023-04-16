f9b2307f1d8df6f57fd7bb9cddd2a2506082c0e2
hackftz <472849306@qq.com>
2023-04-16 10:39:26 +0800

    feat: add

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import { useEffect, useCallback, useMemo, useState } from "react";
+import React, { useEffect, useCallback, useMemo, useState } from "react";
 
 import "./App.css";
 
+function TableData(data) {
+  return (
+    // <>
+    //   {data.map((rec) => (
+    //     <React.Fragment key={rec.id}>
+    //       <td>{rec.hobby}</td>
+    //     </React.Fragment>
+    //   ))}
+    // </>
+    <React.Fragment>
+      <td>John Doe</td>
+      <td>16</td>
+      <td>Developer</td>;
+    </React.Fragment>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {TableData([
+        {
+          id: 1,
+          hobby: "basket",
+        },
+        {
+          id: 2,
+          hobby: "foot",
+        },
+      ])}
+    </div>
+  );
 }
 
 export default App;
